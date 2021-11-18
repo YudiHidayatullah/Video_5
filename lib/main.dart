2f9c71b3d7f163f1d78e6a637df600cc3fb3bcae
YudiHidayatullah <yudihidayatullah00@gmail.com>
2021-11-18 19:58:36 +0800

    First Commit : Video 5

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarRowColumn());
+
+class BelajarRowColumn extends StatelessWidget {
+  const BelajarRowColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Row Column',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: AplikasiKu(),
+    );
+  }
+}
+
+class AplikasiKu extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text('Row & Column'),
+        ),
+        body: Container(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Icon(Icons.alarm), Text('Halo Gan')],
+            )),
+            Container(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Icon(Icons.navigation), Text('Halo Gan')],
+            )),
+          ],
+        )));
+  }
+}
